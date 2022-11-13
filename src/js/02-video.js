af43571b23f8856ef95c5c796bc0ad036426056d
Cherniugov Yevgenii <cherniugov@gmail.com>
2022-11-13 17:55:56 +0200

    Finish 8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const IFRAME = document.querySelector('iframe');
+const player = new Player(IFRAME);
+
+player.on('timeupdate', () => {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+});
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
